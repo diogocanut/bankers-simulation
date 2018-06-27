4b94ec930bd233b00b6b22428430adefa99cfda1
diogocanut <diogocanut@hotmail.com>
2018-06-27 02:02:55 -0300

    added mutex lock on criticial region + random numbers generation

--- main.c
+++ main.c
@@ -3,25 +3,52 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <sys/types.h>
+#include <sys/syscall.h>
 
 #define MAX_THREADS 50
 
-pthread_t thread_id[MAX_THREADS]; 
+pthread_t thread_id[MAX_THREADS];
+pthread_mutex_t lock;
+int *total;
+int *disponiveis;
 
-void *process()
+void *process(int args)
 {
+  printf("thread created, pid: %lu\n", syscall(SYS_gettid));
 
-  printf("thread created\n");
+  int i;
+  int *requisicoes;
+
+  requisicoes = calloc((args-3), sizeof(int));
+
+  pthread_mutex_lock(&lock);
+
+  for(i=0;i<(args-4);i++){
+    requisicoes[i] = (int) rand() % (total[i]+1);
+
+  }
+
+  pthread_mutex_unlock(&lock);
+
+
+  free(requisicoes);
+  requisicoes = NULL;
+
   return NULL;
 }
 
+
+int requisicao_recursos(int pid, int recursos[]){
+
+}
 
 int main(int argc, char *argv[])
 {
 
   int i, n;
-  int *available;
+  int *args = argc;
+  srand(time(NULL));
 
   if(argc < 4){
       printf("Please add the number of threads to the command line\n");
@@ -40,9 +67,11 @@ int main(int argc, char *argv[])
   }
 
   if(!strcmp(argv[3], "-a")){
-    available = malloc((argc-3)*sizeof(int));
+    total = malloc((argc-3)*sizeof(int));
+    disponiveis = malloc((argc-3)*sizeof(int));
     for(i=0;i<(argc-4);i++){
-      available[i] = atoi(argv[i+4]);
+      total[i] = atoi(argv[i+4]);
+      disponiveis[i] = atoi(argv[i+4]);
     }
   }
   else{
@@ -51,10 +80,9 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
-
   for(i=0;i<n;i++)
   {
-    if(pthread_create(&thread_id[i], NULL, process, NULL)) {
+    if(pthread_create(&thread_id[i], NULL, process, args)) {
       fprintf(stderr, "Error creating thread\n");
       return 1;
     }
@@ -66,6 +94,9 @@ int main(int argc, char *argv[])
   }
 
   pthread_exit(NULL);
-  free(available);
+  free(total);
+  total=NULL;
+  free(disponiveis);
+  disponiveis=NULL;
   return 0;
 }
