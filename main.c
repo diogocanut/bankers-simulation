c22a9fc430873a2d1a7fb53a22a8344744e096db
Diogo Canut <canut@wcore.com.br>
2018-06-29 16:33:59 -0300

    lol

--- main.c
+++ main.c
@@ -15,33 +15,39 @@ pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 int done = 0;
 
 int *total;
-int *disponiveis;
+int *alocados;
 int args;
 
 void *process(void* tpid)
 {
   const int pid = (long) tpid;
 
-  int i, var, nextpid = pid;
-  int *recursos;
+  int i, var, menor, nextpid = pid;
+  int *recursos, *max;
 
   recursos = calloc((args-3), sizeof(int));
+  max = calloc((args-3), sizeof(int));
 
   while(1){
     pthread_mutex_lock(&lock);
-    printf("Thread PID %d requisita: ", pid);
+    printf("Thread PID %d max: ", pid);
     for(i=0;i<(args-4);i++){
-      recursos[i] = (int) rand() % (total[i]+1);
-      printf("%d ", recursos[i]);
+      max[i] = (int) rand() % (total[i]+1);
+      if((menor = max[i] - alocados[i])>0)
+        recursos[i] = (int) rand() % (menor+1);
+      else recursos[i] = 0;
+      printf("%d ", max[i]);
     }
     printf("\n");
     pthread_mutex_unlock(&lock);
 
     var = requisicao_recursos(pid, recursos);
     if(var == 0){
+
       sleep(3);
       libera_recursos(pid, recursos);
       sleep(3);
+
     }
     else{
       printf("Thread PID: %d não esta em um estado seguro, ABORTING THREAD...\n", pid);
@@ -62,7 +68,7 @@ int safe_state(int recursos[]){
   int i, var;
   int count = 0;
   for(i=0;i<(args-4);i++){
-    var = disponiveis[i] - recursos[i];
+    var = recursos[i] - alocados[i];
     if(var >=0 && var <= total[i]){
       count++;
     }
@@ -71,33 +77,35 @@ int safe_state(int recursos[]){
 }
 
 int requisicao_recursos(int pid, int recursos[]){
+  pthread_mutex_lock(&lock);
   int i;
   if(safe_state(recursos) == 0){
-    pthread_mutex_lock(&lock);
-    printf("Thread PID: %d requisitou recursos com segurança, DISPONIVEIS:", pid);
+
+    printf("Thread PID: %d requisitou recursos com segurança, ALOCADO:", pid);
     for(i=0;i<(args-4);i++){
-      disponiveis[i] = disponiveis[i] - recursos[i];   
-      printf("%d ", disponiveis[i]);
+      alocados[i] = alocados[i] + recursos[i];   
+      printf("%d ", alocados[i]);
     }
     printf("\n");
     pthread_mutex_unlock(&lock);
     return 0;
   }
   else{
+    pthread_mutex_unlock(&lock);
     return -1;
   }
 }
 
 int libera_recursos(int pid, int recursos[]){
-  int i;
-  printf("Thread PID: %d liberou recursos, DISPONIVEIS:", pid);
   pthread_mutex_lock(&lock);
+  int i;
+  printf("Thread PID: %d liberou recursos, ALOCADO:", pid);
   for(i=0;i<(args-4);i++){
-    disponiveis[i] = disponiveis[i] + recursos[i];   
-    printf("%d ", disponiveis[i]);
+    alocados[i] = alocados[i] - recursos[i];   
+    printf("%d ", alocados[i]);
   }
-  pthread_mutex_unlock(&lock);
   printf("\n");
+  pthread_mutex_unlock(&lock);
   return 0;
 }
 
@@ -123,13 +131,13 @@ int main(int argc, char *argv[])
     printf("Wrong args\n");
     exit(1);
   }
+  alocados = calloc((argc-3), sizeof(int));
 
   if(!strcmp(argv[3], "-a")){
     total = malloc((argc-3)*sizeof(int));
-    disponiveis = malloc((argc-3)*sizeof(int));
     for(i=0;i<(argc-4);i++){
       total[i] = atoi(argv[i+4]);
-      disponiveis[i] = atoi(argv[i+4]);
+
     }
   }
   else{
@@ -149,7 +157,7 @@ int main(int argc, char *argv[])
 
   pthread_mutex_lock(&lock);
 
-  while(done<args-4 ){
+  while(done<n){
       pthread_cond_wait(&cond,&lock);
       break;
     }
@@ -159,7 +167,7 @@ int main(int argc, char *argv[])
 
   free(total);
   total=NULL;
-  free(disponiveis);
-  disponiveis=NULL;
+  free(alocados);
+  alocados=NULL;
   return 0;
 }
